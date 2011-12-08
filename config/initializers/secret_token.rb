8d1770c48b876ca378cfa627ba7a7cfea3774c5e
Hosokawa Hiroyuki <gitrhythm@h2factory.org>
2011-12-08 12:48:09 +0900

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitrhythm::Application.config.secret_token = 'eb080eaa62932ae2a906aab8ca58d18a4307cfc359644a98d1c53bdaf0586b0d58139bf2dd5bca45365a2cea5f6ce6cb30aded4571f74c232438a1776fe10079'
